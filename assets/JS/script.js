406ea0ffdf2eda3671ea7971bdcaa266306864be
Kameron <kfarshchian@gmail.com>
2022-12-01 20:44:49 -0700

    fix script

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -11,7 +11,7 @@ function getApod() {
   }
   
   getApod()
-
+ console.log(getApod);
 
   function getAsteroids() {
     var requestUrl = 'https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=Y8ssRfZuDgzbCeAiyKHGwmeED3ohkeyHHyZc2BrZ';
@@ -25,19 +25,21 @@ function getApod() {
       });
   }
   
-  getAsteroids()
+  console.log();
 
-  function getExoplanet() {
-    var requestUrl = 'https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=cumulative&where=koi_prad<2 and koi_teq>180
-    and koi_teq<303 and koi_disposition like 'CANDIDATE'';
+  getAsteroids(getAsteroids)
+
+//   function getExoplanet() {
+//     var requestUrl = 'https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=cumulative&where=koi_prad<2 and koi_teq>180
+//     and koi_teq<303 and koi_disposition like 'CANDIDATE'';
   
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);     
-      });
-  }
+//     fetch(requestUrl)
+//       .then(function (response) {
+//         return response.json();
+//       })
+//       .then(function (data) {
+//         console.log(data);     
+//       });
+//   }
   
-  getExoplanet()
+//   getExoplanet()
